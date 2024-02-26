388410f8cbfbd09cd424bdc0eb3feb0b8dd86766
Henry Zou <henry.zou@tufts.edu>
2024-02-26 17:46:25 -0500

    Added ghz circuits

---
+++ workspace_experiments/circuits/circuit_files.py
@@ -0,0 +1,27 @@
+import os
+from os import listdir
+from os.path import isfile, join
+from qiskit import QuantumCircuit
+
+
+def get_circuits_from_directory(directory):
+    """ Return a list of QuantumCircuits from the files in the directory. 
+    The files are sorted by the number of lines in the file. """
+
+    # Check if the directory exists, if not raise an error
+    if not os.path.exists(directory):
+        raise ValueError(f'The directory {directory} does not exist')
+
+    # Get all of the files in the directory
+    files = [f for f in listdir(directory) if isfile(join(directory, f))]
+
+    # Sort the files by the number of lines in the file
+    files.sort(key=lambda f: sum(1 for line in open(join(directory, f))))
+
+    # Read the files and return the QuantumCircuits
+    circuits = []
+    for file in files:
+        with open(join(directory, file), 'r') as f:
+            circuits.append(QuantumCircuit.from_qasm_str(f.read()))
+
+    return circuits
